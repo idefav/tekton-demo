aa405fb5e612e3fa2881453ee959a6e19a895984
吴子庶 <wzs2009_hi@126.com>
2020-06-14 11:07:45 +0800

    update Dockerfile

---
+++ shell/install.sh
@@ -0,0 +1,3 @@
+kubectl create secret docker-registry aliyun --namespace=tekton-demo \
+--docker-server=https://registry.cn-hangzhou.aliyuncs.com --docker-username=wzs2009_hi@126.com \
+--docker-password=aV6F91K@D8fg --docker-email=wzs2009_hi@126.com
